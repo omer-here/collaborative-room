b38491f38c20a0aef0a7690e5ade442ae7333940
omer-here <110015265+omer-here@users.noreply.github.com>
2025-02-09 19:27:13 +0500

    minor changes

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
@@ -7,6 +9,17 @@ export default function SignInPage() {
       style={{ backgroundImage: "url('/assets/icons/background.svg')" }}
     >
       <SignIn/>
+      {/* <Link href="https://www.linkedin.com/in/omerafzaal1" target="_blank">
+      <p className="text-xs text-gray-200 font-normal italic underline">
+        <span>
+        <Image 
+        src="/rounded-linkedin.svg"
+        alt="linkedin"
+        width={20}
+        height={20}
+      />
+        </span>in/omerafzaal1</p>
+      </Link> */}
     </main>
   )
 }
